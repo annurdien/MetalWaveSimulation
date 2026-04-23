2edb1bcaa13129cda031f78d56ac8e90e8725211
Annurdien Rasyid <annurdien.dev@gmail.com>
2026-04-23 13:52:28 +0700

    Initial Commit

---
+++ PixelWave/PixelWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelWaveApp.swift
+//  PixelWave
+//
+//  Created by Annurdien Rasyid on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct PixelWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
